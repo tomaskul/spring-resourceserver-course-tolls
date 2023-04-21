e807f59bff4553cc50220a06ff651acedff55193
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-04-21 14:55:43 +0100

    start.spring.io OAuth2 resource server + spring web generated project

---
+++ src/test/java/io/github/tomaskul/resourceserver/ResourceserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tomaskul.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
